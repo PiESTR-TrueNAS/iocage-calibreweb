b3a1969256b71aadd0f8d6754cdbeff354e2f252
Marc Proux <marc.proux@outlook.fr>
2020-10-30 19:03:06 +0100

    Little improvement and reorganization

--- overlay/CalibreWebRc/CPS_Start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-nohup python /CalibreWeb/cps.py &
--- post_install.sh
+++ post_install.sh
@@ -6,3 +6,8 @@ git clone https://github.com/janeczku/calibre-web.git /CalibreWeb
 # Install everything
 pip install --target vendor -r requirements.txt
 
+# Add to the crontab
+echo "@reboot root /CalibreWebRc/CPS_Service start" >> /etc/crontab
+
+# Start the service
+/CalibreWebRc/CPS_Service start
